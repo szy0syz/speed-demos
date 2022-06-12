066fa7ce3f93495af9d67dd4f80bcd8441afdeae
Zhenyu Shi <zhenyu.shi@compass.com>
2022-06-12 11:44:30 +0800

    Update Blog sction

--- highking/src/components/Navbar/Nav.jsx
+++ highking/src/components/Navbar/Nav.jsx
@@ -1,11 +1,8 @@
-import React from 'react';
-import { useState } from 'react';
-import Logo from '../../assets/logo.png';
-import NavLinks from './NavLinks';
-
+import React, { useState } from "react";
+import Logo from "../../assets/logo.png";
+import NavLinks from "./NavLinks";
 const Nav = () => {
   const [open, setOpen] = useState(false);
-  
   return (
     <nav className="flex justify-between">
       <img src={Logo} alt="logo" className="cursor-pointer h-9" />
@@ -13,15 +10,16 @@ const Nav = () => {
         <NavLinks />
       </ul>
       {/* Mobile Nav */}
-      <ul className={`fixed top-0 z-50 flex flex-col w-2/3 h-screen gap-10 pt-20 bg-white shadow-2xl md:hidden 
-        text-medium p-7 duration-500
-        ${open ? 'right-0': 'right-[-100%]'}
-      `}>
+      <ul
+        className={`fixed top-0 z-50 bg-white w-2/3 h-screen shadow-2xl
+      md:hidden flex flex-col gap-10 text-medium  p-7 pt-20 duration-500
+      ${open ? "right-0" : "right-[-100%]"}`}
+      >
         <NavLinks />
       </ul>
 
-      <div className="z-50 text-2xl md:hidden" onClick={() => setOpen(prev => !prev)}>
-        <ion-icon name="menu"></ion-icon>
+      <div className="z-50 text-2xl md:hidden" onClick={() => setOpen(!open)}>
+        <ion-icon name={`${open ? "close" : "menu"}`}></ion-icon>
       </div>
     </nav>
   );
