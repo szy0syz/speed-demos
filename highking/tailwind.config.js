9cd275d5c8fb4dcd041431dc3fa77fe943a0a265
Zhenyu Shi <zhenyu.shi@compass.com>
2022-06-13 08:39:25 +0800

    Update tailwind config

--- highking/tailwind.config.js
+++ highking/tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
         lightprimary: "#ECF3FF",
       },
       backgroundImage: {
-        Hero: "url('assets/background.png')",
+        Hero: "url('/src/assets/background.png')",
       },
     },
   },
